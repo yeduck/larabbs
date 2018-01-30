cba08011ee47c6dbf21d321d4c22467361988e10
yeduck <1217311779@qq.com>
2018-01-30 16:33:27 +0800

    编辑用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    //显示用户资料
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    //编辑用户资料
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+    //保存用户编辑
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
