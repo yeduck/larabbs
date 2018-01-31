4240b057e97fcb21e936bcfcae3563669b1227e9
yeduck <1217311779@qq.com>
2018-01-31 16:10:49 +0800

    消息通知

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,7 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+
+//显示通知消息
+Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
