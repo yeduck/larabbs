dd3b1f67f111b9cc200d2ddea6b9df48cfd1862a
yeduck <1217311779@qq.com>
2018-01-31 13:06:47 +0800

    上传图片

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', '
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
