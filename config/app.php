69dd85ee08eeb8946969b9327b86a489b4c2e94b
yeduck <1217311779@qq.com>
2018-01-31 17:15:54 +0800

    发送邮件

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Naux\Mail\SendCloudServiceProvider::class,
 
         /*
          * Package Service Providers...
