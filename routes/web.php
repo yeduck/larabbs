97b03348b6848c059ae11f489de2ef6e5bd44bc8
yeduck <1217311779@qq.com>
2018-02-01 16:24:12 +0800

    设置首页

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
-
-// Authentication Routes...
+Route::get('/', 'TopicsController@index')->name('root');// Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
